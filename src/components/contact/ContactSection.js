5e240a8ad1609961f85b48d79478a08afc91c11d
robteaw <robteaw@gmail.com>
2021-09-05 00:14:31 -0500

    18. Navbar Fix

--- src/components/contact/ContactSection.js
+++ src/components/contact/ContactSection.js
@@ -24,6 +24,7 @@ export default function ContactSection() {
                     <p>
                       <HiOutlineMail />
                       <a href="mailto:seoapelinks@gmail.com">
+                        {" "}
                         seoapelinks@gmail.com
                       </a>
                     </p>
